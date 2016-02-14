623faf33cdb4a1cb0d70477a1c4978a96445ee8b
Wildfoot <wild.foot.yee.tzwu@gmail.com>
2016-02-14 21:11:05 +0800

    add django

---
+++ Django/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/common/programming_language/djangovirtual/Class_net/Django/bin/python3.4
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
